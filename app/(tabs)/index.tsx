53261fa75c9fc4b4823b749d39c41ae228942d24
Nama Anda <wicaksono.aldi22@gmail.com>
2024-11-25 19:32:15 +0700

    push ketiga warna background

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fcfc03',
+    backgroundColor: '#33333',
   },  
   text: {
     fontSize: 30,
